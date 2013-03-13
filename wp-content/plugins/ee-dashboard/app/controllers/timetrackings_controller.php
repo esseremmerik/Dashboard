c1ce606cb3a3e56cd545d93312ba33b6182c07af
esseremmerik <dev@presteren.nu>
2013-03-13 22:42:59 +0100

    Er is een functie gemaakt die een object opslaat wanneer je de timer aanzet. Dit object bevat informatie over je lopende tijd die bewaard moet blijven wanneer je je tabblad/browser sluit.

--- wp-content/plugins/ee-dashboard/app/controllers/timetrackings_controller.php
+++ wp-content/plugins/ee-dashboard/app/controllers/timetrackings_controller.php
@@ -752,15 +752,39 @@ class TimetrackingsController extends MvcPublicController {
 		update_ownership();
 		return;
 	}
+	/**
+	 * Function to save a object which contains information about a running time of a employee.
+	 * <br />The idea is that when a employee starts the timer, this function is called.
+	 * <br />The next information should be in the post:
+	 * <br />- The value of the text field where you put your activity
+	 * <br />- The task id which is the task id from Solve360
+	 * <br />- The task id from WordPress. This is the primary key of the task table.
+	 * <br />- The tasklist id from WordPress. This is identical to the one from Solve360
+	 */
 	function saveTimeObj(){
 		$autocomplete 		= $_POST['autocomplete'];
 		$solve360_taskid 	= $_POST['solve360_taskid'];
 		$wp_taskid			= $_POST['wp_taskid'];
 		$tasklistid			= $_POST['tasklistid'];
 		$details			= $_POST['details'];
-		echo 'test123';
-		//wp user id
-		//timestamp current time
+		
+		$this->load_model('Ownership');
+		$current_solve_user = $this->current_solve_user();
+		$solve360_user_id = $current_solve_user->id;
+		
+		$data = array(
+			'id' 			  => '',
+			'timestamp_start' => time(),
+			'wp_user_id'	  => $solve360_user_id,
+			'autocomplete'	  => $autocomplete,
+			'solve360_task_id'=> $solve360_taskid,
+			'wp_task_id'	  => $wp_taskid,
+			'tasklist_id'	  => $tasklistid,
+			'beschrijving'	  => $details,
+		);
+		global $wpdb;
+		$wpdb->insert('wp_ee_current_timerecording', $data);
+		
 		exit();
 	}
 }
